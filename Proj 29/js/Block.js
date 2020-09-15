9a54337f0c156992865710c4dd93395bc9f69b41
codeSahana <69361539+codeSahana@users.noreply.github.com>
2020-09-15 07:07:17 -0400

    Add files via upload

---
+++ Proj 29/js/Block.js
@@ -0,0 +1,39 @@
+class Block extends Ground{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+      
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+
+        if (this.body.speed<3){
+          super.display();
+          debugger
+        }
+        else {
+          push();
+          World.remove(world, this.body);
+          this.Visiblity = this.Visiblity-5;
+          tint(255, this.Visiblity);
+          image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+          pop();
+        }
+      }
+      }
